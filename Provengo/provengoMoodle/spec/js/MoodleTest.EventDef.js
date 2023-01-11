2d171ac63f06e66db7dbf4358d33c2bd9040a25a
noamazulay274 <100525699+noamazulay274@users.noreply.github.com>
2023-01-12 01:11:21 +0200

    final version

--- Provengo/provengoMoodle/spec/js/MoodleTest.EventDef.js
+++ Provengo/provengoMoodle/spec/js/MoodleTest.EventDef.js
@@ -1,137 +1,132 @@
 /* @Provengo summon selenium */
 
-// /**
-//  *  The ComposeQuery event defines the selenium actions for writing the query that was given in the parameter e.
-//  */
-// defineEvent(SeleniumSession, "Login_user", function (session, e) {
-//     session.click("//*[contains(text(),'Log in')]");
-//     session.writeText("//*[@id='username']", e.UserName);
-//     session.writeText("//*[@name='password' and @type='password']", e.Password);
-//     session.click("//*[@id='loginbtn']");
-// });
-//
-// /**
-//  * The StartSearch event defines the selenium action for clicking the search button.
-//  */
-// defineEvent(SeleniumSession, "Navigate_to_course_teacher", function (session, e) {
-//     session.click("//*[contains(text(),'My courses') and @role='menuitem']");
-//     session.click("//*[@class='multiline' and contains(text(),'" + e.CourseName + "')]");
-// });
-// defineEvent(SeleniumSession, "Navigate_to_course_student", function (session, e) {
-//     // waitFor(Any("EndOfAction").and(Any({eventName:"Create_DB"})))
-//     session.click("//*[contains(text(),'My courses') and @role='menuitem']")
-//     session.click("//*[@class='multiline' and contains(text(),'" + e.CourseName + "')]")
-// });
-//
-// defineEvent(SeleniumSession, "Create_DB", function (session, e) {
-//     session.click("//*[@type='checkbox' and @name='setmode']");
-//     session.click("//*[contains(text(),'Add an activity or resource')]");
-//     session.click("//*[contains(text(),'Database') and @class='optionname clamp-2']");
-//     session.writeText("//*[@id='id_name']", e.DBName);
-//     session.click("//*[@id='id_submitbutton']");
-//     session.click("//*[@type='checkbox' and @name='setmode']");
-// });
-//
-// defineEvent(SeleniumSession, "Add_fields", function (session, e) {
-//     for (let i = 0; i < e.Fields.length; i++) {
-//         session.click("//option[@value='text']");
-//         session.writeText("//*[@id='name']", e.Fields[i]);
-//         session.click("//*[@type='submit' and @value='Add']");
-//
-//     }
-//     session.click("//*[@class='nav more-nav nav-tabs']/li[4]/a");
-//     session.click("//*[@type='submit' and @value='Save template']");
-//     session.click("//*[@class='nav more-nav nav-tabs']/li[3]/a");
-// })
-//
-//
-// defineEvent(SeleniumSession, "Navigate_to_DB", function (session, e) {
-//     session.click("//*[@data-activityname='" + e.DBName + "']//a");
-// })
-//
-//
-// defineEvent(SeleniumSession, "Navigate_to_add_entry", function (session, e) {
-//     // waitFor(Any("EndOfAction").and(Any({eventName: "Add_fields"})))
-//     session.click("//*[@type='submit' and @class='btn btn-primary']");
-// })
-//
-//
-//
-// defineEvent(SeleniumSession, "Student_add_new_entry", function (session, e) {
-//     for (let i = 1; i < e.Entry.length+1; i++) {
-//         session.writeText("//*[@class='mod-data-default-template ##approvalstatusclass##']/tbody/tr["+i+"]/td/div/input", e.Entry[i-1]);
-//     }
-// })
-//
-// defineEvent(SeleniumSession, "Submit_entry", function (session, e) {
-//     session.click("//*[@type='submit' and @value='Save']");
-// })
-// defineEvent(SeleniumSession, "Teacher_delete_one_Field", function (session, e) {
-//     session.click("//*[@class='nav more-nav nav-tabs']/li[3]/a");
-//     session.click("//*[@id='region-main']/div[2]/div[2]/table/tbody/tr["+e.FiledNum+"]/td[5]/a[2]/i");
-//     session.click("//*[@type='submit' and text()='Continue']");
-// })
-//
-//
-// defineEvent(SeleniumSession, "Check_delete_filed", function (session, e) {
-//     session.assertText("//*[@class='alert alert-success alert-block fade in  alert-dismissible']", "Field deleted",TextAssertions.modifiers.Contains)
-// })
-// defineEvent(SeleniumSession, "Check_after_submit_entry", function (session, e) {
-//      session.assertText("//*[@class='alert alert-success alert-block fade in  alert-dismissible']", "Field deleted",TextAssertions.modifiers.Contains)
-// })
-//
-// defineEvent(SeleniumSession, "Check_before_submit_entry", function (session, e) {
-//     session.assertText("//*[@Class='alert alert-danger alert-block fade in  alert-dismissible']", "No entries in database")
-// })
-//
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
+/**
+ *  The ComposeQuery event defines the selenium actions for logging in to the system.
+ *  it takes an object that contains the username and password of the user.
+ */
 defineEvent(SeleniumSession, "Login_user", function (session, e) {
+    // click on the login button
+    session.click("//*[contains(text(),'Log in')]");
+    // type the username
+    session.writeText("//*[@id='username']", e.UserName);
+    // type the password
+    session.writeText("//*[@name='password' and @type='password']", e.Password);
+    // click on the submit button
+    session.click("//*[@id='loginbtn']");
 });
-
 
 /**
- * The StartSearch event defines the selenium action for clicking the search button.
+ * The Navigate_to_course_teacher event defines the selenium action for navigating to the course.
+ * it takes the course name as a parameter
  */
 defineEvent(SeleniumSession, "Navigate_to_course_teacher", function (session, e) {
+    // click on the My courses button
+    session.click("//*[contains(text(),'My courses') and @role='menuitem']");
+    // click on the specified course
+    session.click("//*[@class='multiline' and contains(text(),'" + e.CourseName + "')]");
 });
-defineEvent(SeleniumSession, "Create_DB", function (session, e) {
+/**
+ * The Navigate_to_course_student event defines the selenium action for navigating to the course.
+ * it takes the course name as a parameter
+ */
+defineEvent(SeleniumSession, "Navigate_to_course_student", function (session, e) {
+    session.click("//*[contains(text(),'My courses') and @role='menuitem']")
+    session.click("//*[@class='multiline' and contains(text(),'" + e.CourseName + "')]")
+});
 
+
+defineEvent(SeleniumSession, "Create_DB", function (session, e) {
+    // Enables editing mode for the course
+    session.click("//*[@type='checkbox' and @name='setmode']");
+    // Clicks on the "Add an activity or resource" button
+    session.click("//*[contains(text(),'Add an activity or resource')]");
+    // Clicks on the "Database" button
+    session.click("//*[contains(text(),'Database') and @class='optionname clamp-2']");
+    // Writes the name of the database in the "Name" field
+    session.writeText("//*[@id='id_name']", e.DBName);
+    // Clicks the "Save and return to course" button
+    session.click("//*[@id='id_submitbutton']");
+    // Disables editing mode for the course
+    session.click("//*[@type='checkbox' and @name='setmode']");
 });
+
+
 
 defineEvent(SeleniumSession, "Add_fields", function (session, e) {
+    for (let i = 0; i < e.Fields.length; i++) {
+        session.click("//option[@value='text']");
+        session.writeText("//*[@id='name']", e.Fields[i]);
+        session.click("//*[@type='submit' and @value='Add']");
 
+    }
+    session.click("//*[@class='nav more-nav nav-tabs']/li[4]/a");
+    session.click("//*[@type='submit' and @value='Save template']");
+    session.click("//*[@class='nav more-nav nav-tabs']/li[3]/a");
 })
 
-
+defineEvent(SeleniumSession, "Add_fields", function (session, e) {
+    // loop through all fields in the e.Fields array
+    for (let i = 0; i < e.Fields.length; i++) {
+        session.click("//option[@value='text']"); // select the 'text' option for the field
+        session.writeText("//*[@id='name']", e.Fields[i]); // enter the name of the field
+        session.click("//*[@type='submit' and @value='Add']"); // submit the field
+    }
+    //  Save the fields
+    session.click("//*[@class='nav more-nav nav-tabs']/li[4]/a");
+    session.click("//*[@type='submit' and @value='Save template']");
+    session.click("//*[@class='nav more-nav nav-tabs']/li[3]/a");
+})
 defineEvent(SeleniumSession, "Navigate_to_DB", function (session, e) {
-})
-
+    // click on the database link that has the name of the database
+    session.click("//*[@data-activityname='" + e.DBName + "']//a");
+});
 
 defineEvent(SeleniumSession, "Navigate_to_add_entry", function (session, e) {
-
-})
+    // click on the "Add entry" button
+    session.click("//*[@type='submit' and @class='btn btn-primary']");
+});
 
 
 defineEvent(SeleniumSession, "Student_add_new_entry", function (session, e) {
-
+    //loops through the array of entries passed in e.Entry and enters each value into the database input field
+    for (let i = 1; i < e.Entry.length+1; i++) {
+        session.writeText("//*[@class='mod-data-default-template ##approvalstatusclass##']/tbody/tr["+i+"]/td/div/input", e.Entry[i-1]);
+    }
 })
 
 defineEvent(SeleniumSession, "Submit_entry", function (session, e) {
+    //submits the entry by clicking the 'Save' button
+    session.click("//*[@type='submit' and @value='Save']");
 })
+
 defineEvent(SeleniumSession, "Teacher_delete_one_Field", function (session, e) {
-
-
+    // navigate to the form fields tab
+    session.click("//*[@class='nav more-nav nav-tabs']/li[3]/a");
+    // select the field to delete using the index passed in e.FiledNum and click on delete button
+    session.click("//*[@id='region-main']/div[2]/div[2]/table/tbody/tr["+e.FiledNum+"]/td[5]/a[2]/i");
+    // confirm delete
+    session.click("//*[@type='submit' and text()='Continue']");
 })
 
-defineEvent(SeleniumSession, "Navigate_to_course_student", function (session, e) {
 
-});
 defineEvent(SeleniumSession, "Check_after_submit_entry", function (session, e) {
-})
+    // This function logs in a student user, navigates to a specific course, and navigates to a specific database
+    session.login_user({UserName: StudentUserName, Password: StudentPassword});
+    session.navigate_to_course_student({CourseName: coursename});
+    session.navigate_to_DB({DBName: dbname});
+    // This loop iterates through the 'e.Entry' array and asserts that each entry is present on the page
+    for (let i = 0; i < e.Entry.length; i++) {
+        session.assert("//*[contains(text(),'" +e.Entry[i]+"')]", e.Entry[i]);
+    }
+});
 
 defineEvent(SeleniumSession, "Check_before_submit_entry", function (session, e) {
-})
+    // This function logs in a student user, navigates to a specific course, and navigates to a specific database
+    session.login_user({UserName: StudentUserName, Password: StudentPassword});
+    session.navigate_to_course_student({CourseName: coursename});
+    session.navigate_to_DB({DBName: dbname});
+    // Asserts that the message "No entries in database" is present on the page
+    session.assertText("//*[@Class='alert alert-danger alert-block fade in  alert-dismissible']", "No entries in database");
+});
+
 
--- Provengo/provengoMoodle/spec/js/MoodleTest.story.js
+++ Provengo/provengoMoodle/spec/js/MoodleTest.story.js
@@ -1,5 +1,18 @@
 /* @provengo summon selenium */
 
+/*
+ * This script automates interactions between a student and teacher
+ * using a database through Selenium. The script contains four stories:
+ *
+ * 1. Student fill entries in DB
+ * 2. Teacher delete DB filed
+ * 3. Teacher create DB and add fields
+ * 4. AssertDataBaseFields
+ *
+ * To use this script, run the script with Provengo installed and the Selenium browser driver running.
+ * You may also need to edit the variables at the top of the script to match your desired database and login credentials.
+ *
+ */
 
 // The fields that will be used in the database
 const fields = ['FirstName', 'LastName', 'ID'];
@@ -24,7 +37,6 @@ const entry = ['Noam', 'Azulay', '212988232'];
 // The options for fields that the teacher can delete
 const Del_Options = [1, 2, 3];
 
-
 /**
  * This story logs in as a student, navigates to the course and database, and adds an entry to the database.
  */
@@ -32,14 +44,21 @@ story('Student fill entries in DB', function () {
 
     on(Any("EndOfAction").and(Any({eventName: "Add_fields"})), function () {
         with (new SeleniumSession("Student").start('http://localhost')) {
+            // Interrupt the story if the teacher deleted one field before.
             interrupt(Any("EndOfAction").and(Any({eventName: "Teacher_delete_one_Field"})), function () {
+                // Login as student
                 login_user({UserName: StudentUserName, Password: StudentPassword})
+                // Navigate to the course
                 navigate_to_course_student({CourseName: coursename})
+                // Navigate to the database
                 navigate_to_DB({DBName: dbname})
+                // Navigate to the add entry page
                 navigate_to_add_entry()
             })
             on(Any("EndOfAction").and(Any({eventName: "Teacher_delete_one_Field"})), function () {
+                // Add new entry
                 student_add_new_entry({Entry: entry});
+                // Submit entry
                 submit_entry()
             })
 
@@ -51,62 +70,66 @@ story('Student fill entries in DB', function () {
  * This story logs in as a teacher, navigates to the course and database, and deletes a field from the database.
  */
 story('teacher delete DB filed', function () {
-
+    // Run this story when the "Add_fields" event occurs
     on(Any("EndOfAction").and(Any({eventName: "Add_fields"})), function () {
+        // Start a Selenium session with the role "Teacher"
         with (new SeleniumSession("Teacher").start('http://localhost')) {
+            // Login as teacher
             login_user({UserName: TeacherUserName, Password: TeacherPassword})
+            // Navigate to the teacher view of the course
             navigate_to_course_teacher({CourseName: coursename})
+            // Navigate to the database
             navigate_to_DB({DBName: dbname})
-            teacher_delete_one_Field({FiledNum: 1})
+            // Delete one field from the database
+            teacher_delete_one_Field({FiledNum: choose(Del_Options)})
         }
     })
 
 })
 
-// choose(Del_Options)
+// create the DB and add fields to it.
 story('teacher create DB and add fields ', function () {
+    // Start a Selenium session with the role "TeacherCreate"
     with (new SeleniumSession("TeacherCreate").start('http://localhost')) {
+        // Login as teacher
         login_user({UserName: TeacherUserName, Password: TeacherPassword})
+        // Navigate to the teacher view of the course
         navigate_to_course_teacher({CourseName: coursename})
+        // Create the database
         create_DB({DBName: dbname})
+        // Add fields to the database
         add_fields({Fields: fields})
     }
 })
-//
-// story('AssertDataBaseFields', function () {
-//     on(Any("EndOfAction").and(Any({eventName: "Add_fields"})), function () {
-//         with (new SeleniumSession("AssertSession").start('http://localhost')) {
-//             on(Any("EndOfAction").and(Any({eventName: "Submit_entry"})), function () {
-//                 login_user({UserName: StudentUserName, Password: StudentPassword})
-//                 navigate_to_course_student({CourseName: coursename})
-//                 navigate_to_DB({DBName: dbname})
-//                 check_after_submit_entry()
-//             })
-//             interrupt(Any("EndOfAction").and(Any({eventName: "Submit_entry"})), function () {
-//                 check_before_submit_entry()
-//             })
-//         }
-//     })
-//
-// })
+
+
+story('AssertDataBaseFields', function () {
+    // Run this story when the "Add_fields" event occurs
+    on(Any("EndOfAction").and(Any({eventName: "Add_fields"})), function () {
+        // Start a Selenium session with the role "AssertSession"
+        with (new SeleniumSession("AssertSession").start('http://localhost')) {
+            // Run this inner function when the "Submit_entry" event occurs
+            on(Any("EndOfAction").and(Any({eventName: "Submit_entry"})), function () {
+                // check the database after submitting the entry
+                check_after_submit_entry()
+            })
+            // Interrupt the above function if the "Submit_entry" event occurred before "Navigate_to_add_entry"
+            interrupt(Any("EndOfAction").and(Any({eventName: "Submit_entry"})), function () {
+                // check the database before submitting the entry
+                check_before_submit_entry({Entry: entry})
+            })
+        }
+    })
+})
+
 
 // @provengo summon constraints
-// Constraints.after(Any("EndOfAction").and(Any({eventName: "Navigate_to_add_entry"})))
-//     .require(Any("EndOfAction").and(Any({eventName: "Student_add_new_entry"})))
-//     .eventually();
 
-// Constraints.after(Any("EndOfAction").and(Any({eventName: "Teacher_delete_one_Field"})))
-//     .require(Any("EndOfAction").and(Any({eventName: "navigate_to_add_entry"}))).eventually();
-
-
-// Constraints.after(Any("EndOfAction").and(Any({eventName: "Create_DB"})))
-//     .require(Any("EndOfAction").and(Any({eventName: "Navigate_to_course_student"})))
-//     .eventually();
-
+// Block the "Teacher_delete_one_Field" event until "Navigate_to_add_entry" is occurred
 Constraints.block(Any("EndOfAction").and(Any({eventName: "Teacher_delete_one_Field"})))
     .until(Any("EndOfAction").and(Any({eventName: "Navigate_to_add_entry"})));
 
-
+// Block the "Teacher_delete_one_Field" event until "Navigate_to_add_entry" is occurred
 Constraints.block(Any({eventName: "Teacher_delete_one_Field"}))
     .until(Any("EndOfAction").and(Any({eventName: "Navigate_to_add_entry"})));
 
